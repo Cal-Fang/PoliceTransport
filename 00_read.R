ea90de1dff73306a9e427c45396732437dbad4d0
Cal Chengqi Fang <chengqi@uchicago.edu>
2024-07-08 11:12:48 -0500

    Update the script to download 2024 records.

--- 00_read.R
+++ 00_read.R
@@ -35,40 +35,59 @@ memory.limit(30000000)                  # this is needed on some PCs to increase
 
 require(tidyverse)
 require(data.table)
+require(httr)       # For url requesting and parsing
 
 ## ---------------------------
 prefix <- "https://www.gunviolencearchive.org/export-finished/download?uuid=436a0477-9747-4396-a32c-71086dcce9bf&filename=public%3A//"
 
+# Write a function to handle downloading
+download <- function(end, pre=prefix){
+  # Make the url address
+  url <- paste0(prefix, end)
+  # Use httr to make the GET request
+  response <- GET(url)
+  # Parse the content as a CSV
+  content <- content(response, "text")
+  
+  result <- fread(content, check.names=TRUE)
+  return(result)
+}
+
+# 2024
+K2024 <- download("export-f5f8702b-eab3-46d1-a11c-d5dee7f36893.csv")
+I2024 <- download("export-dce90536-6168-4aea-9856-6c27a311ccb0.csv")
+
 # 2023
-K2023 <- fread(paste0(prefix, "export-e9cadfb9-6eac-43ce-9804-17c2893f01a6.csv"), check.names=TRUE)
-I2023 <- fread(paste0(prefix, "export-849e34d3-4fda-48d0-80d1-53bfaa95b410.csv"), check.names=TRUE) 
+K2023 <- download("export-e4a66ab9-84d5-4588-a323-62811dae72c3.csv")
+I2023 <- download("export-7eee5b83-3689-4af9-914d-744bb7d585a2.csv") 
 
 # 2022
-K2022 <- fread(paste0(prefix, "export-ac8e6404-47e0-4750-b741-21ce08a41bd6.csv"), check.names=TRUE) 
-I2022 <- fread(paste0(prefix, "export-aaaef91b-0fbf-4a0a-8219-c6b5f387ce6e.csv"), check.names=TRUE) 
+K2022 <- download("export-6aa98895-6724-4133-92c0-41667dcc7c3e.csv") 
+I2022 <- download("export-43c352f5-436b-4ab3-9d32-f4d2449b1e00.csv") 
 
 # 2021
-K2021 <- fread(paste0(prefix, "export-00261eef-de98-4efc-9bc9-e09ea1c03cd2.csv"), check.names=TRUE) 
-I2021 <- fread(paste0(prefix, "export-fbf17a22-aab7-4c53-99bf-f268152fffc2.csv"), check.names=TRUE) 
+K2021 <- download("export-fb6c41a3-ece1-4f4b-a853-937cf1b246ab.csv") 
+I2021 <- download("export-d7eea84b-a7fa-4d4d-9b02-c8d5b5fb1540.csv") 
 
 # 2020
-K2020 <- fread(paste0(prefix, "export-2ee1216a-f11b-4cf2-895d-3e81f55058bc.csv"), check.names=TRUE) 
-I2020 <- fread(paste0(prefix, "export-5ec9be8d-b570-4e1d-b8d8-e8c787d15bce.csv"), check.names=TRUE) 
+K2020 <- download("export-2d3b7e84-46a4-43d2-89c4-eb23b60386bb.csv") 
+I2020 <- download("export-27db3609-e110-42c7-a484-5829770a9477.csv") 
 
 # 2019
-K2019 <- fread(paste0(prefix, "export-126979de-4456-4960-b761-daa6c2272756.csv"), check.names=TRUE) 
-I2019 <- fread(paste0(prefix, "export-d39f0b61-43b5-4e45-b5ee-e2c421c02e34.csv"), check.names=TRUE) 
+K2019 <- download("export-53676e90-6bac-45c4-9d64-061c9d1e4ebf.csv") 
+I2019 <- download("export-a6e7ff9b-fbb1-4eef-bf21-ab812663279f.csv") 
 
 # 2018
-A2018 <- fread(paste0(prefix, "export-eb4c5abd-c297-4966-8dc6-435cc744a672.csv"), check.names=TRUE) 
+A2018 <- download("export-e51ddca1-8bbb-4d2b-90b3-f2c8a6b7fd05.csv") 
 
 
 # Save all raw data file for later cleaning and merging
-save(K2023, I2023,
+save(K2024, I2024,
+     K2023, I2023,
      K2022, I2022,
      K2021, I2021,
      K2020, I2020,
      K2019, I2019,
      A2018, 
-     file="data/rawrecords.Rdata")
+     file="data/recordsRaw.Rdata")
 
